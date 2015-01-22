4c1d5aa1cbe64a6ca6c7665bd624b7dbae3a3d58
Rafaël <rafael.caso2@gmail.com>
2015-01-21 19:05:44 -0500

    Clean Marv1n-test module

--- Marv1n-code/src/main/java/org/Marv1n/code/Room.java
+++ Marv1n-code/src/main/java/org/Marv1n/code/Room.java
@@ -4,7 +4,7 @@ package org.Marv1n.code;
  * Created by maxime on 14/01/2015.
  */
 public class Room {
-    public boolean isAvailable() {
+    public Boolean IsReserved(){
         return false;
     }
 }
