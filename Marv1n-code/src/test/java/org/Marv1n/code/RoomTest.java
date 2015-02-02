aae1a6170e8fa3115368b9aa2e904c5192a5d33c
MILOU8 <mickael.loubriat.1@ulaval.ca>
2015-02-02 21:43:34 +0100

    Refartoring de la request

--- Marv1n-code/src/test/java/org/Marv1n/code/RoomTest.java
+++ Marv1n-code/src/test/java/org/Marv1n/code/RoomTest.java
@@ -6,12 +6,15 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class RoomTest {
+
     final static private int NUMBER_OF_SEATS = 25;
     private Room room;
+    private Request request;
 
     @Before
     public void initializeNewRoom() {
         this.room = new Room(NUMBER_OF_SEATS);
+        this.request = new Request();
     }
 
     @Test
@@ -21,15 +24,14 @@ public class RoomTest {
 
     @Test
     public void newRoomWhenReserveIsReserved() {
-        this.room.book(new Request());
+        this.room.book(request);
         assertTrue(this.room.isBooked());
     }
 
     @Test
     public void newRoomWhenReserveHaveTheRightReservation() {
-        Request test = new Request();
-        this.room.book(test);
-        assertEquals(test, this.room.getRequest());
+        this.room.book(request);
+        assertEquals(request, this.room.getRequest());
     }
 
     @Test
