4b1752a0522051e6423050e6ff5c047a744caad5
Francois Renaud <nate@nate.fr>
2015-02-02 11:09:06 -0500

    Ajout contenu exception

--- Marv1n-code/src/main/java/org/Marv1n/code/exception/NoRoomAvailableException.java
+++ Marv1n-code/src/main/java/org/Marv1n/code/exception/NoRoomAvailableException.java
@@ -4,5 +4,8 @@ package org.Marv1n.code.exception;
  * Created by Rafaël on 27/01/2015.
  */
 public class NoRoomAvailableException extends RuntimeException {
+    public NoRoomAvailableException() {
+        super("No room are available");
+    }
 
 }
