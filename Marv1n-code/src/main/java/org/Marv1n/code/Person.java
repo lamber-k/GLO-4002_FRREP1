909fc8b8867181ed4f934b6497f1858565335c54
lamber_k <kev993@hotmail.com>
2015-02-16 20:37:23 -0500

    Create Person Representation

---
+++ Marv1n-code/src/main/java/org/Marv1n/code/Person.java
@@ -0,0 +1,33 @@
+package org.Marv1n.code;
+
+import java.util.UUID;
+
+public class Person {
+    private String  mailAddress;
+    private UUID    personID;
+
+    public Person(String mailAddress) {
+        this.personID = UUID.randomUUID();
+        this.mailAddress = mailAddress;
+    }
+
+    public String   getMailAddress() {
+        return this.mailAddress;
+    }
+
+    public UUID     getID() {
+        return this.personID;
+    }
+
+    @Override
+    public boolean  equals(Object rhs)
+    {
+        if (rhs == null) {
+            return false;
+        }
+        if (rhs instanceof Person) {
+            return this.personID.equals(((Person) rhs).personID);
+        }
+        return false;
+    }
+}
