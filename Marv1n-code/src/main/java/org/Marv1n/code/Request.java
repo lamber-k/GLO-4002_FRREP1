5de3327d30e286fc91ff091a6c740fcd4d9cc557
lamber_k <kev993@hotmail.com>
2015-02-16 19:09:59 -0500

    Request: add UUID

--- Marv1n-code/src/main/java/org/Marv1n/code/Request.java
+++ Marv1n-code/src/main/java/org/Marv1n/code/Request.java
@@ -1,16 +1,24 @@
 package org.Marv1n.code;
 
+import java.util.UUID;
+
 public class Request {
 
+    private UUID requestID;
     private Integer numberOfSeatsNeeded = 0;
     private Integer priority = 0;
 
     public Request(Integer numberOfSeatsNeeded) {
+        this.requestID = UUID.randomUUID();
         this.numberOfSeatsNeeded = numberOfSeatsNeeded;
     }
 
     public Integer getNumberOdSeatsNeeded() {
         return this.numberOfSeatsNeeded;
+    }
+
+    public UUID getRequestID() {
+        return (this.requestID);
     }
 
     public void setNumberOfSeatsNeeded(Integer numberOfSeatsNeeded) {
