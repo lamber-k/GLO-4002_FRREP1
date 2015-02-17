9a33763f8f708bdc066f298c460e62edc9f7c148
François <nate@nate.fr>
2015-02-16 20:41:48 -0500

    Add test for ReservableRepository

--- Marv1n-code/src/test/java/org/Marv1n/code/ReservableRepositoryTest.java
+++ Marv1n-code/src/test/java/org/Marv1n/code/ReservableRepositoryTest.java
@@ -3,22 +3,30 @@ package org.Marv1n.code;
 import org.Marv1n.code.Repository.ReservableRepository;
 import org.Marv1n.code.Reservable.Reservable;
 import org.junit.Before;
+import org.junit.Test;
 
-import java.util.UUID;
+import java.util.List;
 
+import static junit.framework.Assert.assertFalse;
 import static org.mockito.Mockito.mock;
 
 public class ReservableRepositoryTest {
     private ReservableRepository reservableRepository;
     private Reservable mockReservable;
-    private UUID mockReservableID;
 
     @Before
     public void setUp() throws Exception {
 
         this.reservableRepository = new ReservableRepository();
         this.mockReservable = mock(Reservable.class);
-        this.mockReservableID = UUID.randomUUID();
     }
 
+    @Test
+    public void respositoryContainsReservableWhenFindAllThenReturnAllReservable() throws Exception {
+        this.reservableRepository.create(mockReservable);
+
+        List<Reservable> results = this.reservableRepository.findAll();
+
+        assertFalse(results.isEmpty());
+    }
 }
