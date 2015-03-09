1bdec1d979a7fe7bceb673989861af3aab231ffe
Rafaël <rafael.caso2@gmail.com>
2015-03-09 16:37:37 -0400

    Base notifier

---
+++ Marv1n-code/src/test/java/org/Marv1n/code/org/Marv1n/code/notifier/NotifierMailTest.java
@@ -0,0 +1,13 @@
+package org.Marv1n.code.org.Marv1n.code.notifier;
+
+import org.junit.Test;
+
+/**
+ * Created by Rafaël on 16/02/2015.
+ */
+public class NotifierMailTest {
+    @Test
+    public void createNotifierMail() {
+        new NotifierMail();
+    }
+}
