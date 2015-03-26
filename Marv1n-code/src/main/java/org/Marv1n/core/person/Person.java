fba8a71482b98ab97526d8ddff7493fb0ab9282c
lamber_k <kev993@hotmail.com>
2015-03-25 20:24:54 -0400

    - move email validator to Person

--- Marv1n-code/src/main/java/org/Marv1n/core/person/Person.java
+++ Marv1n-code/src/main/java/org/Marv1n/core/person/Person.java
@@ -1,5 +1,7 @@
 package org.Marv1n.core.person;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 import java.util.UUID;
 
 public class Person {
@@ -8,18 +10,33 @@ public class Person {
     private final String mailAddress;
     private final boolean admin;
 
-    public Person(String mailAddress) {
+    public Person(String mailAddress) throws AddressException {
+        if (!validate(mailAddress)) {
+            throw new AddressException();
+        }
         this.personID = UUID.randomUUID();
         this.mailAddress = mailAddress;
         this.admin = false;
     }
 
-    public Person(String mailAddress, boolean admin) {
-        personID = UUID.randomUUID();
+    public Person(String mailAddress, boolean admin) throws AddressException {
+        if (!validate(mailAddress)) {
+            throw new AddressException();
+        }
+        this.personID = UUID.randomUUID();
         this.mailAddress = mailAddress;
         this.admin = admin;
     }
 
+    private static boolean validate(String email) {
+        try {
+            InternetAddress emailAddress = new InternetAddress(email);
+            emailAddress.validate();
+        } catch (AddressException | NullPointerException exception) {
+            return false;
+        }
+        return true;
+    }
     public String getMailAddress() {
         return mailAddress;
     }
